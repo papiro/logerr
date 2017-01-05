17f444e31bc3562e2a34d38a7f253a4f5450cbcf
papiro <pierre.pirault@outlook.com>
2017-01-05 02:27:41 -0500

    support for user-defined log-level added

--- index.js
+++ index.js
@@ -2,46 +2,54 @@
 
 const 
   fs     =  require('fs'),
-  stream =  require('stream'),
-  path   =  require('path')
-,
-  callingModulePath = path.dirname(module.parent.filename)
+  stream =  require('stream')
 ;
+
+let logl = 0 // no logging by default
 
 function toWritable (handle) {
   if (handle instanceof stream.Writable) {
     return handle
   } else {
-    return fs.createWriteStream(path.resolve(callingModulePath, handle), { flags: 'a' }) 
+    return fs.createWriteStream(handle, { flags: 'a' }) 
   }
 }
 
 class Log extends console.Console {
-  constructor (_out, _err) {
-    const out = toWritable(_out)
-    const err = toWritable(_err)
-    super(out, err)
+  constructor (out, err) {
+    super(toWritable(out), err ? toWritable(err) : undefined)
   }
   log (...args) {
-    args.unshift('LOG: ')
-    super.log(...args)
     process.env.DEBUG && console.log(...args)
+    if (logl < 1) return
+    args.unshift('L ')
+    super.log(...args)
   }
   info (...args) {
-    args.unshift('INFO: ')
-    super.info(...args)
     process.env.DEBUG && console.info(...args)
+    if (logl < 2) return
+    args.unshift('I ')
+    super.info(...args)
   }
   error (...args) {
-    args.unshift('ERROR: ')
-    super.error(...args)
     process.env.DEBUG && console.error(...args)
+    if (logl < 3) return
+    args.unshift('E ')
+    super.error(...args)
   }
   warn (...args) {
-    args.unshift('WARNING: ')
-    super.warn(...args)
     process.env.DEBUG && console.warn(...args)
+    if (logl < 4) return
+    args.unshift('W ')
+    super.warn(...args)
   }
 }
 
-module.exports = Log
+Log.prototype.log.newline = () => { console.log(this) }
+
+module.exports = logLevel => {
+  logl = logLevel || 0
+  // change module.exports
+  module.exports = Log
+  return Log
+}
