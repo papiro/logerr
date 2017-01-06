f42467c620daf66fcd2d7ee2452713d59e230312
papiro <pierre.pirault@outlook.com>
2017-01-06 01:13:36 -0500

    - added 'error' event handler to writeStreams - added parameter to serve as base to resolve log files to - slowly becoming a member of awning...

--- index.js
+++ index.js
@@ -2,18 +2,22 @@
 
 const 
   fs     =  require('fs'),
-  stream =  require('stream')
+  stream =  require('stream'),
+  path   =  require('path')
 ,
   timezoneOffset = (new Date()).getTimezoneOffset() * 60000
 ;
 
-let logl = 0 // no logging by default
+let logl, root
 
 function toWritable (handle, rotate) {
   if (handle instanceof stream.Writable) {
     return handle
   } else {
-    return fs.createWriteStream(handle, { flags: rotate ? 'w' : 'a' }) 
+    return fs.createWriteStream(path.resolve(root, handle), { flags: rotate ? 'w' : 'a' }) 
+      .on('error', err => {
+        throw err
+      })
   }
 }
 
@@ -50,9 +54,11 @@ class Log extends console.Console {
 
 Log.prototype.log.newline = () => { console.log(this) }
 
-module.exports = logLevel => {
-  logl = logLevel || 0
-  // change module.exports
+module.exports = ({ logLevel = 0, logsPath = process.cwd() }) => {
+  logl = logLevel
+  root = logsPath
+  // change module.exports for future requires
   module.exports = Log
+  // return Log for the first require
   return Log
 }
