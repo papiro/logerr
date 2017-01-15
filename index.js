2d2c57e876e18baa5828ddfe5865ab3b1b2c88be
papiro <pierre.pirault@outlook.com>
2017-01-15 14:19:06 -0500

    - Fixed log levels - added "trace - removed log instance-specifc options cause they were clobbering the   logging of objects (such as error objects)

--- index.js
+++ index.js
@@ -32,31 +32,28 @@ class Log extends console.Console {
     super(toWritable(out, options), err ? toWritable(err, options) : undefined)
   }
   common (level, prefix, method, ...args) {
-    // log-write instant-specific options
-    // date is prepended to every log write by default
-    let options = { date: true }
-    if (typeof args[args.length-1] === 'object') {
-      Object.assign(options, args.pop())
-    }
     process.env.DEBUG && console.log(...args)
     if (logl < level) return
-    args.unshift(prefix, options.date ? new Date(Date.now() - timezoneOffset).toISOString() : '')
+    args.unshift(prefix, new Date(Date.now() - timezoneOffset).toISOString())
     super[method](...args)
   }
   debug (...args) {
     process.env.DEBUG && console.log(...args) 
   }
   log (...args) {
-    this.common(1, 'L', 'log', ...args)
+    this.common(3, 'L', 'log', ...args)
   }
   info (...args) {
-    this.common(2, 'I', 'info', ...args)
+    this.common(4, 'I', 'info', ...args)
   }
   error (...args) {
-    this.common(3, 'E', 'error', ...args)
+    this.common(1, 'E', 'error', ...args)
   }
   warn (...args) {
-    this.common(4, 'W', 'warn', ...args)
+    this.common(2, 'W', 'warn', ...args)
+  }
+  trace (...args) {
+    this.common(1, 't', 'trace', ...args)
   }
 }
 
