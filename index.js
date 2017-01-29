2dc72a694674ff4ed51a53d7fee68789f604fa68
papiro <pierre.pirault@outlook.com>
2017-01-28 19:32:53 -0500

    added debugging and started log levels at 0

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 const 
   fs = require('fs'), stream = require('stream'), path = require('path'), util = require('util')
 ,
+  debug = util.debuglog('logerr')
+,
   timezoneOffset = (new Date()).getTimezoneOffset() * 60000
 ;
 
@@ -21,6 +23,7 @@ function toWritable (handle) {
 
 class Log extends console.Console {
   constructor (out, err, options = {}) {
+    debug(`logerr instance being constructed with out:::${out} and err:::${err}`)
     // options specific to this log
     if (typeof err !== 'string' && !( err instanceof stream.Writable )) {
       options = err 
@@ -29,18 +32,19 @@ class Log extends console.Console {
     if (!options.namespace || typeof options.namespace !== 'string') {
       throw new Error(`log for ${out} missing a valid <String> namespace for util.debuglog`)
     }
-
-    this.debuglog = require('util').debuglog(options.namespace)
     // handle optional second parameter (separate file for stderr)
     super(toWritable(out, options), err ? toWritable(err, options) : undefined)
+
+    this.debuglog = require('util').debuglog(options.namespace)
   }
   common (level, prefix, method, ...args) {
-    if (process.env.NODE_DEBUG.toLowerCase() === 'true') {
+    if (process.env.NODE_DEBUG && process.env.NODE_DEBUG.toLowerCase() === 'true') {
       console.log(...args)
     } else {
       this.debuglog(...args)
     }
-    if (logl < level) return
+    debug(`is the global logLevel (${logl}) <= the level of this log type? (${method}:::${level})`)
+    if (logl <= level) return
     args.unshift(prefix, new Date(Date.now() - timezoneOffset).toISOString())
     super[method](...args)
   }
@@ -48,19 +52,19 @@ class Log extends console.Console {
     process.env.DEBUG && console.log(...args) 
   }
   log (...args) {
-    this.common(3, 'L', 'log', ...args)
+    this.common(2, 'L', 'log', ...args)
   }
   info (...args) {
-    this.common(4, 'I', 'info', ...args)
+    this.common(3, 'I', 'info', ...args)
   }
   error (...args) {
-    this.common(1, 'E', 'error', ...args)
+    this.common(0, 'E', 'error', ...args)
   }
   warn (...args) {
-    this.common(2, 'W', 'warn', ...args)
+    this.common(1, 'W', 'warn', ...args)
   }
   trace (...args) {
-    this.common(1, 't', 'trace', ...args)
+    this.common(0, 't', 'trace', ...args)
   }
 }
 
@@ -69,6 +73,7 @@ Log.prototype.log.newline = () => { console.log(this) }
 module.exports = ({ logLevel = 0, logsPath = process.cwd() }) => {
   logl = logLevel
   root = logsPath
+  debug(`logerr run with logLevel ${logl} and with logsPath ${root}`)
   // change module.exports for future requires
   module.exports = Log
   // return Log for the first require
