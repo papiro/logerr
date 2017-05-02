7cd7eaed929338f8bf75f12986b529b86a31827b
papiro <pierre.pirault@outlook.com>
2017-05-02 07:29:31 -0400

    working on refactor of logerr to not use console object

--- index.js
+++ index.js
@@ -10,18 +10,7 @@ const
 
 let logl, root
 
-function toWritable (handle) {
-  if (handle instanceof stream.Writable) {
-    return handle
-  } else {
-    return fs.createWriteStream(path.resolve(root, handle), { flags: 'a' }) 
-      .on('error', err => {
-        throw err
-      })
-  }
-}
-
-class Log extends console.Console {
+class Log {
   constructor (out, err, options = {}) {
     debug(`logerr instance being constructed with out:::${out} and err:::${err}`)
     // options specific to this log
@@ -32,24 +21,24 @@ class Log extends console.Console {
     if (!options.namespace || typeof options.namespace !== 'string') {
       throw new Error(`log for ${out} missing a valid <String> namespace for util.debuglog`)
     }
-    // handle optional second parameter (separate file for stderr)
-    super(toWritable(out, options), err ? toWritable(err, options) : undefined)
-
-    this.debuglog = require('util').debuglog(options.namespace)
+    Object.assign(this, {
+      outLog: fs.createWriteStream(out, { flags: 'a', autoClose: false }),
+      errLog: err && fs.createWriteStream(err, { flags: 'a', autoClose: false }),
+      debuglog: require('util').debuglog(options.namespace)
+    })
   }
   common (level, prefix, method, ...args) {
-    if (process.env.NODE_DEBUG && process.env.NODE_DEBUG.toLowerCase() === 'true') {
-      console.log(...args)
-    } else {
-      this.debuglog(...args)
-    }
+    this.debuglog(...args)
+
     debug(`is the global logLevel (${logl}) >= the level of this log type? (${method}:::${level})`)
     if (logl >= level) return
     args.unshift(prefix, new Date(Date.now() - timezoneOffset).toISOString())
+
     super[method](...args)
   }
   log (...args) {
     this.common(2, 'L', 'log', ...args)
+    fs.write
   }
   info (...args) {
     this.common(3, 'I', 'info', ...args)
